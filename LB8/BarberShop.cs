0cc52de49331191ae05372dc6ef098852ecff6e0
Uchiha-Ihor <ihorychiha@gmail.com>
2025-04-06 12:11:15 +0200

    LB8 BarberShop

---
+++ LB8/BarberShop.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LB8
+{
+    internal class BarberShop
+    {
+        private int number_of_seats;
+
+        private int visitors = 0;
+
+        private Thread Barber;
+
+        private readonly object lockObject = new object();
+
+        internal BarberShop(int number_of_seats)
+        { 
+            this.number_of_seats = number_of_seats;
+
+            Barber = new Thread(BarberWorking);
+
+            Barber.Start();
+        }
+
+        internal void AddVisitors()
+        {
+            lock (lockObject)
+            {
+                Console.WriteLine("Enter numbers of visitors in Barber Shop");
+                visitors = Convert.ToInt32(Console.ReadLine());
+
+                if (visitors > number_of_seats)
+                {
+                    visitors = number_of_seats;
+                }
+                else
+                {
+                    Console.WriteLine("No free seats, visitor left.");
+                }
+            }
+        }
+
+        void BarberWorking()
+        {
+            int current_visitors;
+
+            while (true)
+            {
+                lock (lockObject)
+                {
+                    current_visitors = visitors;
+                }
+                if (current_visitors > 0)
+                {
+                    for (int i = 0; i < current_visitors; i++)
+                    {
+                        Console.WriteLine("Barber start cut visitor " + i);
+                        Thread.Sleep(1000);
+                        Console.WriteLine("Barber end cut visitor " + i);
+                        Thread.Sleep(200);
+                    }
+                    lock (lockObject)
+                    {
+                        visitors = 0;
+                    }
+
+                    Console.WriteLine(Environment.NewLine + "Barber sleep" + Environment.NewLine);
+                }
+                else
+                {
+                    Thread.Sleep(200);
+
+                }
+
+            }
+        }
+
+    }
+}
---
+++ LB8/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace LB8
+{
+    public class Program
+    {
+        //Thread BarberShop;
+
+        static void Main()
+        {
+            Console.WriteLine("Enter numbers of seats in Barber Shop");
+            int number_of_seats = Convert.ToInt32(Console.ReadLine());
+
+            BarberShop barberShop = new BarberShop(number_of_seats);
+
+            while (true)
+            {
+                barberShop.AddVisitors();
+            }
+        }
+
+
+
+
+    }
+}
